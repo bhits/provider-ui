d990403554461b5e26bfc92c5c4f9676fdedb606
JiahaoLi <Jiahao.Li@feisystems.com>
2017-06-05 15:30:33 -0400

    Create initial provider search component
    #14043: As a developer I will have implemented the Provider UI to manage patient consent

---
+++ client/src/app/provider/provider-search/provider-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'c2s-provider-search',
+  templateUrl: './provider-search.component.html',
+  styleUrls: ['./provider-search.component.scss']
+})
+export class ProviderSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/provider/provider.module.ts
+++ client/src/app/provider/provider.module.ts
@@ -1,15 +1,17 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 
-import {ProviderRoutingModule} from "./provider-routing.module";
+import {ProviderRoutingModule, routedComponents} from "./provider-routing.module";
 import {ProviderListComponent} from "./provider-list/provider-list.component";
+import {SharedModule} from "app/shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    ProviderRoutingModule
+    ProviderRoutingModule,
+    SharedModule
   ],
-  declarations: [ProviderListComponent],
+  declarations: [routedComponents, ProviderListComponent],
   exports: [ProviderListComponent]
 })
 export class ProviderModule {
