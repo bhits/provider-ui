351ba741019f365a24798b1dedb74d6cf3ddd36d
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-06-05 18:32:20 -0400

    Use ngx-uploader to post file xml file to the backend
    #13772 - As a developer, I will have enhanced data segmentation for FHIR resources (part 2)

--- client/src/app/consent/segmentation/segmentation.component.ts
+++ client/src/app/consent/segmentation/segmentation.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, ViewChild } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {UploadOutput, UploadInput, UploadFile, humanizeBytes} from 'ngx-uploader';
+
+
+import {TokenService} from "../../security/shared/token.service";
+import {UploadOutputType} from "../shared/upload-output-type.enum";
+import {SegmentationRequest} from "../shared/segmentation-request";
+import {ConsentService} from "../shared/consent.service";
 
 @Component({
   selector: 'c2s-segmentation',
@@ -8,20 +15,31 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class SegmentationComponent implements OnInit {
   public segmentationFrom: FormGroup;
+  public files: UploadFile[];
+  public uploadInput: EventEmitter<UploadInput>;
+  public humanizeBytes: Function;
 
-  constructor( private formBuilder: FormBuilder,) { }
+
+  constructor( private formBuilder: FormBuilder,
+               private consentService: ConsentService,
+               private tokenService: TokenService) {
+    // local uploading files array
+    this.files = [];
+    // input events, we use this to emit data to ngx-uploader
+    this.uploadInput = new EventEmitter<UploadInput>();
+    this.humanizeBytes = humanizeBytes;
+  }
 
   ngOnInit() {
     this.segmentationFrom = this.buildSegementationForm();
   }
 
-
   private buildSegementationForm(): FormGroup{
     return this.formBuilder.group({
-      from: [null,
+      intermediaryNpi: [null,
         []
       ],
-      to: [null,
+      recipientNpi: [null,
         []
       ],
       purposeOfUse: [null,
@@ -31,5 +49,49 @@ export class SegmentationComponent implements OnInit {
         []
       ],
     });
+  }
+
+  onUploadOutput(output: UploadOutput): void {
+    if (output.type === UploadOutputType.ADDED_TO_QUEUE.toString()) {
+      this.files.push(output.file); // add file to array when added
+    } else if (output.type === UploadOutputType.UPLOADING.toString()) {
+      // update current data in files array for uploading file
+      const index = this.files.findIndex(file => file.id === output.file.id);
+      this.files[index] = output.file;
+    } else if (output.type === UploadOutputType.REMOVED.toString()) {
+      // remove file from array when removed
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    } else if (output.type === UploadOutputType.DONE.toString()) {
+      // Handle download of filed
+      console.log(output.file.response);
+    }
+  }
+
+  segmentDocument(): void {
+    const formModel = this.segmentationFrom.value;
+    this.uploadInput.emit(this.prepareUploadInputObject(formModel));
+  }
+
+  private prepareSegmentationRequestObject(formContro: any):any{
+    let segmentationRequest: SegmentationRequest = new SegmentationRequest();
+    segmentationRequest.recipientNpi = formContro.recipientNpi;
+    segmentationRequest.intermediaryNpi = formContro.intermediaryNpi;
+    segmentationRequest.purposeOfUse = formContro.purposeOfUse;
+    segmentationRequest.patientIdRoot = "";
+    segmentationRequest.patientIdExtension = "";
+    return segmentationRequest;
+  }
+
+  private prepareUploadInputObject(formModel:any): UploadInput{
+    let uploadInput: UploadInput = {
+        type: 'uploadAll',
+        fieldName: 'file',
+        url: this.consentService.getPepSegmentationDocumentUrl(),
+        method: 'POST',
+        data: this.prepareSegmentationRequestObject(formModel),
+        concurrency: 1, // set sequential uploading of files with concurrency 1
+        headers: this.tokenService.createAuthorizationHeaderObject()
+    };
+    return uploadInput;
   }
 }
--- client/src/app/consent/shared/consent.service.ts
+++ client/src/app/consent/shared/consent.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
+import {ApiUrlService} from "../../shared/api-url.service";
 
 @Injectable()
 export class ConsentService {
 
-  constructor() { }
+  private pepSegmentDocumentUrl: string = this.apiUrlService.getPepUrl().concat("/segmentDocument");
 
+  constructor(private http: Http,
+              private apiUrlService: ApiUrlService) { }
+
+  getPepSegmentationDocumentUrl(): string{
+    return this.pepSegmentDocumentUrl;
+  }
 }
