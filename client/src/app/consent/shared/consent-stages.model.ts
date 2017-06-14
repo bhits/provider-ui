db82c44419835f95fd67a65f09667a8470b5f13e
JiahaoLi <Jiahao.Li@feisystems.com>
2017-06-14 19:10:20 -0400

    Implement i18n for manage consents dialog
    #14043: As a developer I will have implemented the Provider UI to manage patient consent

--- client/src/app/consent/shared/consent-stages.model.ts
+++ client/src/app/consent/shared/consent-stages.model.ts
@@ -4,59 +4,59 @@ import {ConsentStageOptionKey} from "./consent-stage-option-key.enum";
 export const CONSENT_STAGES: ConsentStage[] = [
   {
     consentStage: "SAVED",
-    text: "IN PROGRESS",
+    text: "CONSENT.CARD.CONSENT_STAGES.SAVED_STAGE.STAGE_TEXT",
     icon: "edit",
     color: "accent",
     options: [
       {
         key: ConsentStageOptionKey.EDIT,
         icon: "edit",
-        text: "Edit This Consent",
+        text: "CONSENT.CARD.CONSENT_STAGES.SAVED_STAGE.EDIT_THIS_CONSENT_OPTION_TEXT",
         routerLink: '/consents'
       },
       {
         key: ConsentStageOptionKey.DOWNLOAD_SAVED_PDF,
         icon: "search",
-        text: "Preview This Consent",
+        text: "CONSENT.CARD.CONSENT_STAGES.SAVED_STAGE.PREVIEW_THIS_CONSENT_OPTION_TEXT",
         isMethod: true
       },
       {
         key: ConsentStageOptionKey.DELETE,
         icon: "delete_forever",
-        text: "Delete This Consent",
+        text: "CONSENT.CARD.CONSENT_STAGES.SAVED_STAGE.DELETE_THIS_PROVIDER_OPTION_TEXT",
         isMethod: true
       }]
   },
   {
     consentStage: "SIGNED",
-    text: "SIGNED",
+    text: "CONSENT.CARD.CONSENT_STAGES.SIGNED_STAGE.STAGE_TEXT",
     icon: "check_circle",
     color: "primary",
     options: [
       {
         key: ConsentStageOptionKey.DOWNLOAD_SIGNED_PDF,
         icon: "file_download",
-        text: "View Signed Consent",
+        text: "CONSENT.CARD.CONSENT_STAGES.SIGNED_STAGE.VIEW_SIGNED_CONSENT_OPTION_TEXT",
         isMethod: true
       }
     ]
   },
   {
     consentStage: "REVOKED",
-    text: "REVOKED",
+    text: "CONSENT.CARD.CONSENT_STAGES.REVOKED_STAGE.STAGE_TEXT",
     icon: "cancel",
     color: "warn",
     options: [
       {
         key: ConsentStageOptionKey.DOWNLOAD_SIGNED_PDF,
         icon: "file_download",
-        text: "View Signed Consent",
+        text: "CONSENT.CARD.CONSENT_STAGES.REVOKED_STAGE.VIEW_SIGNED_CONSENT_OPTION_TEXT",
         isMethod: true
       },
       {
         key: ConsentStageOptionKey.DOWNLOAD_REVOKED_PDF,
         icon: "file_download",
-        text: "View Signed Revocation",
+        text: "CONSENT.CARD.CONSENT_STAGES.REVOKED_STAGE.VIEW_SIGNED_REVOCATION_OPTION_TEXT",
         isMethod: true
       }
     ]
