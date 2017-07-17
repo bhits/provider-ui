aac2731e6607e8e6fd03ae38239f431899887bc6
Shruti Rao <shruti.rao@feisystems.com>
2017-07-17 15:40:19 -0400

    Add activity history component
    #14613:As a developer I will create consent activity history for Providers

---
+++ client/src/app/history/activity-history/activity-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'c2s-activity-history',
+  templateUrl: './activity-history.component.html',
+  styleUrls: ['./activity-history.component.scss']
+})
+export class ActivityHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
